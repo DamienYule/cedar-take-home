ed6931f54b15bd8f15c099335f1c6988a4ba513c
DamienYule <damienyule@gmail.com>
2022-02-17 03:15:35 -0500

    small changes to list item

--- front-end/src/components/appointmentItem/AppointmentItem.js
+++ front-end/src/components/appointmentItem/AppointmentItem.js
@@ -19,20 +19,23 @@ function AppointmentItem({
   };
 
   return (
-    <ListGroup.Item
-      onClick={handleSelect}
-      as="li"
-      className="d-flex justify-content-between align-items-start aptItem"
-    >
-      <div className="ms-2 me-auto">
-        <div className="fw-bold">{apt?.doctor} </div>
-        <div className="fw-mute">{apt?.patient} </div>
-        <div>{date}</div>
-      </div>
-      <div className="id" >
-        <div className="id__text">id : </div> <div className="id__number">{apt?.id}</div> 
-      </div>
-    </ListGroup.Item>
+    <div className="aptItem">
+      <ListGroup.Item
+        onClick={handleSelect}
+        as="li"
+        className="d-flex justify-content-between align-items-start remove_extra_background"
+      >
+        <div className="ms-2 me-auto">
+          <div className="fw-bold">{apt?.doctor} </div>
+          <div className="fw-mute">{apt?.patient} </div>
+          <div>{date}</div>
+        </div>
+        <div className="id">
+          <div className="id__text">id : </div>{" "}
+          <div className="id__number">{apt?.id}</div>
+        </div>
+      </ListGroup.Item>
+    </div>
   );
 }
 
